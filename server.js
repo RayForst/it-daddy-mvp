62e638979770fd0583e9b0252950919a30950b72
Sergej Kalpakov <sergeikalpakov@gmail.com>
2018-09-21 04:49:50 +0300

    Route hotfix

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get('/sitemap.xml', (req, res) => {
   res.sendFile(`${__dirname}/sitemap.xml`)
 })
 
-app.get('/robots.xml', (req, res) => {
+app.get('/robots.txt', (req, res) => {
   res.sendFile(`${__dirname}/robots.txt`)
 })
 
