5d90f6fa7467ae8f1aff713b5daba1ba249afe54
Sergej Kalpakov <sergeikalpakov@gmail.com>
2019-05-08 08:44:24 +0300

    sendmail change

--- server.js
+++ server.js
@@ -4,18 +4,12 @@ const nodemailer = require('nodemailer')
 const favicon = require('serve-favicon')
 const fs = require('fs')
 const path = require('path')
-const GoogleTokenProvider = require('refresh-token').GoogleTokenProvider
+const sgTransport = require('nodemailer-sendgrid-transport')
 
 const config = JSON.parse(fs.readFileSync('config.json'))
 const app = express()
 const htmlPath = `${__dirname}/static/`
 const urlencodedParser = bodyPsarer.urlencoded({ extended: false })
-
-const tokenProvider = new GoogleTokenProvider({
-  refresh_token: config.refreshToken,
-  client_id: config.clientId,
-  client_secret: config.clientSecret,
-})
 
 app.use(express.static('static'))
 
@@ -73,31 +67,20 @@ app.get('/robots.txt', (req, res) => {
 app.post('/contact', urlencodedParser, (req, res) => {
   if (!req.body) res.statusCode(404)
 
-  tokenProvider.getToken((err, token) => {
-    // token will be a valid access token.
+  var options = {
+    auth: {
+      api_key: config.smtpToken
+    }
+  }
 
-    if (!err) {
-      const transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-          type: 'OAuth2',
-          password: 'holyfuckagain',
-          user: 'sergeikalpakov@gmail.com',
-          clientId: config.clientId,
-          clientSecret: config.clientSecret,
-          refreshToken: config.refreshToken,
-          accessToken: token,
-        },
-      })
+  const transporter = nodemailer.createTransport(sgTransport(options))
 
-      const HelperOptions = {
-        from: '"It daddy" <admin@itdaddy.ca>',
-        to: config.mail,
-        subject: 'New client!',
-        text: `Text from contact form${JSON.stringify(req.body)}`,
-        html: `<h1>Text from contact form</h1>
+  const HelperOptions = {
+    from: '"It daddy" <admin@itdaddy.ca>',
+    to: config.mail,
+    subject: 'New client!',
+    text: `Text from contact form${JSON.stringify(req.body)}`,
+    html: `<h1>Text from contact form</h1>
       <div>Client Name: ${req.body.name}</div>
       <div>Client Email: ${req.body.email}</div>
       <div>Client phone: ${req.body.phone}</div>
@@ -106,22 +89,18 @@ app.post('/contact', urlencodedParser, (req, res) => {
       <div>Prefered Contact type: ${req.body.contactType}</div>
       <div>Prefered Contact time: ${req.body.contactTime}</div>
     `, // html body
-      }
+  }
 
-      transporter.sendMail(HelperOptions, (error, info) => {
-        if (error) console.log(error)
-        else console.log('Mail send Success')
+  transporter.sendMail(HelperOptions, (error, info) => {
+    if (error) console.log(error)
+    else console.log('Mail send Success')
 
-        transporter.close()
-      })
+    transporter.close()
+  })
 
-      res.json({
-        type: 'success',
-        response: 'Thank you for your message. It has been sent.',
-      })
-    } else {
-      console.log(err)
-    }
+  res.json({
+    type: 'success',
+    response: 'Thank you for your message. It has been sent.',
   })
 })
 
